cc3d8c1882e7ed136c6247583e1151b43a5f2fbd
Silvina Staniszewski <silvina.stani@gmail.com>
2023-04-01 20:49:26 -0300

    adding maps directions

--- Components/Direction.jsx
+++ Components/Direction.jsx
@@ -4,10 +4,24 @@ import styles from "../styles/Footer.module.css";
 const Direction = () => {
   return (
     <div className={styles.direction}>
-      <p className={styles.Localidad}>Escobar</p>
-      <p className={styles.p}>Mitre 694</p>
-      <p className={styles.Localidad}>Savio</p>
-      <p className={styles.p}>Boulevard 5 de Junio 928. Galería Local 2</p>
+      <a
+        className={styles.a}
+        href="https://goo.gl/maps/gagVMKDNddp37E8S9"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p className={styles.Localidad}>Escobar</p>
+        <p className={styles.p}>Mitre 694</p>
+      </a>
+      <a
+        className={styles.a}
+        href="https://goo.gl/maps/mGdFukmv3fXkcceJ7"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p className={styles.Localidad}>Savio</p>
+        <p className={styles.p}>Boulevard 5 de Junio 928. Galería Local 2</p>
+      </a>
     </div>
   );
 };
